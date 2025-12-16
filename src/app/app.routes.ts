376bfdfc9f587dab98c8592081e19bea8bf45d56
Agurla Prashanth <Prashanth.Agurla@laurusis.net>
2025-12-16 10:43:06 +0530

    created navbar and footer in app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,7 @@ import { StudentList } from './student-list/student-list';
 
 export const routes: Routes = [
     {path:'',component: StudentList},
-    {path:'student',component: StudentList}
+    {path:'students',component: StudentList},
+    {path: '',redirectTo: 'employees', pathMatch: 'full'}
+
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StudentList } from "./student-list/student-list";
 import {HttpClientModule} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentList,HttpClientModule],
+  imports: [RouterOutlet,HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
